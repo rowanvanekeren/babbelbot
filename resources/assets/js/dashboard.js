e617a140604de955a1704e5b7929a819ae2159e5
rowanvanekeren <rowanvanekeren@hotmail.com>
2017-10-30 00:20:03 +0100

    input update animation

--- resources/assets/js/dashboard.js
+++ resources/assets/js/dashboard.js
@@ -1,19 +1,105 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     passwordHint();
+
+    testFunction();
 
 //end of document ready function
 });
 
-function passwordHint(){
+function passwordHint() {
     var passwordIcon = '.see-password-input>i';
-    $(passwordIcon).click(function(e){
-        if( $(this).parent().children('input').attr('type') == 'text'){
+    $(passwordIcon).click(function (e) {
+
+        $(this).parent().children('input').focus();
+        if ($(this).parent().children('input').attr('type') == 'text') {
             $(this).parent().children('input').attr('type', 'password');
-        } else if($(this).parent().children('input').attr('type') == 'password' ){
+        } else if ($(this).parent().children('input').attr('type') == 'password') {
             $(this).parent().children('input').attr('type', 'text');
         }
         $(this).toggleClass('fa-eye-slash');
 
     });
+
+}
+
+
+function testFunction() {
+    var inpWrapperClass = '.input-wrapper';
+    var inpIconElemClass = '.input-saving-overlay';
+    var inpElemClass = '.input-wrapper input';
+
+    var shrinkClass = 'shrink-input-wapper';
+
+    $(window).resize(function () {
+        //  $(inpWrapperClass).css('width', '100%');
+
+        $('.processing:not(.disable-shrink)').css('width', '100%').css('width', 'calc(100% - 25px)');
+        $('.input-wrapper:not(.processing)').css('width', '100%');
+
+
+    });
+    $(inpElemClass).focus(function () {
+
+        var currentParent = $(this).parent();
+        var cuurentInput = $(this);
+
+        if (currentParent.hasClass('processing') && !currentParent.hasClass('disable-shrink') &&
+            (cuurentInput.attr('disabled') == false || typeof cuurentInput.attr('disabled') == 'undefined')) {
+            currentParent.animate({'width': (currentParent.width() + 25)}, 100, 'linear', function () {
+                currentParent.removeClass('processing');
+                currentParent.children(inpIconElemClass).addClass('hidden');
+                currentParent.children(inpIconElemClass).addClass('fa-repeat').removeClass('fa-check');
+                console.log('i added space');
+            });
+        } else if (currentParent.hasClass('processing') && currentParent.hasClass('disable-shrink') &&
+            (cuurentInput.attr('disabled') == false || typeof cuurentInput.attr('disabled') == 'undefined')) {
+            currentParent.removeClass('processing');
+            currentParent.children(inpIconElemClass).addClass('hidden');
+            currentParent.children(inpIconElemClass).addClass('fa-repeat').removeClass('fa-check');
+        }
+    });
+    $(inpElemClass).keyup(function (e) {
+        if (e.keyCode == 13) {
+            var currentParent = $(this).parent();
+            var cuurentInput = $(this);
+            if (!currentParent.hasClass('disable-shrink') && !currentParent.hasClass('processing')) {
+                currentParent.addClass('processing');
+                cuurentInput.attr('disabled', true);
+                currentParent.animate({'width': (currentParent.width() - 25)}, 100, 'linear', function () {
+                    setTimeout(function () {
+                        currentParent.children(inpIconElemClass).removeClass('hidden');
+
+                        setTimeout(function () {
+                            currentParent.children(inpIconElemClass).removeClass('fa-repeat').addClass('fa-check');
+                            cuurentInput.attr('disabled', false);
+                        }, 1000);
+                    }, 150);
+
+                });
+            } else if (!currentParent.hasClass('processing')) {
+
+                currentParent.addClass('processing');
+                cuurentInput.attr('disabled', true);
+
+                currentParent.children(inpIconElemClass).removeClass('hidden');
+
+                setTimeout(function () {
+                    currentParent.children(inpIconElemClass).removeClass('fa-repeat').addClass('fa-check');
+                    cuurentInput.attr('disabled', false);
+                }, 1000);
+            }
+
+        }
+    });
+
+    //function activateInputLoader(element)
+
+    //function changeInputLoaderState(element , succes/error , message (optional) )
+
+    //function closeInputLoader(element);
+
+    //function watchloaderchange() <- not sure yet
+
+    //focus and onchange fix
 }
--- resources/views/apps.blade.php
+++ resources/views/apps.blade.php
@@ -5,220 +5,256 @@
 
         <div class="row top-row-navigation">
             <div class="col-md-6 col-md-offset-3">
-            <div class="col-md-12 align-right">
-                <button class="main-btn"> Nieuwe App</button></div>
+                <div class="col-md-12 align-right">
+                    <button class="main-btn"> Nieuwe App</button>
+                </div>
             </div>
-            </div>
+        </div>
 
         <div class="row">
             <div class="col-md-6 col-md-offset-3">
-                <div class="col-md-12 app-wrapper" >
+                <div class="col-md-12 app-wrapper">
                     <div class="app-top-section">
-                        <input type="text"  class='inp-head-name' value="App naam 1"> <span><i class="fa fa-times" aria-hidden="true"></i></span>
+                        <div class="input-wrapper disable-shrink">
+                            <input type="text" class='inp-head-name' value="App naam 1">
+                            <i class="fa fa-repeat input-saving-overlay hidden"></i>
+                        </div>
+                            <span><i class="fa fa-times " aria-hidden="true"></i></span>
+
                     </div>
                     <div class="app-center-section">
                         <div class="form-group">
                             <label for="inp-access-token">
                                 Wit.ai access token
                             </label>
-                            <div class="see-password-input">
-                            <i class="fa fa-eye" aria-hidden="true"></i>
-                            <input id="inp-access-token" class="" type="password">
+
+                            <div class="input-wrapper see-password-input">
+                                <i class="fa fa-eye password-eye-icon" aria-hidden="true"></i>
+                                <input id="inp-access-token" class="inp-loading" type="password" >
+                                <i class="fa fa-repeat input-saving-overlay hidden"></i>
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="inp-dev-token">
                                 Wit.ai developers token
                             </label>
-                            <div class="see-password-input">
-                                <i class="fa fa-eye" aria-hidden="true"></i>
+
+                            <div class="input-wrapper see-password-input">
+                                <i class="fa fa-eye password-eye-icon" aria-hidden="true"></i>
                                 <input id="inp-dev-token" class="" type="password">
+                                <i class="fa fa-repeat input-saving-overlay hidden"></i>
                             </div>
                         </div>
                     </div>
                     <div class="app-footer-section">
-                        <button class="main-btn">Embedden</button> <button class="main-btn">Selecteer</button>
+                        <button class="main-btn">Embedden</button>
+                        <button class="main-btn">Selecteer</button>
                     </div>
                 </div>
 
-                <div class="col-md-12 app-wrapper" >
+                <div class="col-md-12 app-wrapper">
                     <div class="app-top-section">
-                        <input type="text" value="App naam 1"> <span><i class="fa fa-times" aria-hidden="true"></i></span>
+                        <div class="input-wrapper disable-shrink">
+                            <input type="text" class='inp-head-name' value="App naam 1">
+                            <i class="fa fa-repeat input-saving-overlay hidden"></i>
+                        </div>
+                        <span><i class="fa fa-times " aria-hidden="true"></i></span>
+
                     </div>
                     <div class="app-center-section">
                         <div class="form-group">
                             <label for="inp-access-token">
                                 Wit.ai access token
                             </label>
-                            <div class="see-password-input">
-                                <i class="fa fa-eye" aria-hidden="true"></i>
-                                <input id="inp-access-token" class="" type="password">
+
+                            <div class="input-wrapper see-password-input">
+                                <i class="fa fa-eye password-eye-icon" aria-hidden="true"></i>
+                                <input id="inp-access-token" class="inp-loading" type="password" >
+                                <i class="fa fa-repeat input-saving-overlay hidden"></i>
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="inp-dev-token">
                                 Wit.ai developers token
                             </label>
-                            <div class="see-password-input">
-                                <i class="fa fa-eye" aria-hidden="true"></i>
+
+                            <div class="input-wrapper see-password-input">
+                                <i class="fa fa-eye password-eye-icon" aria-hidden="true"></i>
                                 <input id="inp-dev-token" class="" type="password">
+                                <i class="fa fa-repeat input-saving-overlay hidden"></i>
                             </div>
                         </div>
                     </div>
                     <div class="app-footer-section">
-                        <button class="main-btn">Embedden</button> <button class="main-btn">Selecteer</button>
+                        <button class="main-btn">Embedden</button>
+                        <button class="main-btn">Selecteer</button>
                     </div>
                 </div>
 
-                <div class="col-md-12 app-wrapper" >
+                <div class="col-md-12 app-wrapper">
                     <div class="app-top-section">
-                        <h2>App naam 1</h2> <span><i class="fa fa-times" aria-hidden="true"></i></span>
+                        <div class="input-wrapper disable-shrink">
+                            <input type="text" class='inp-head-name' value="App naam 1">
+                            <i class="fa fa-repeat input-saving-overlay hidden"></i>
+                        </div>
+                        <span><i class="fa fa-times " aria-hidden="true"></i></span>
+
                     </div>
                     <div class="app-center-section">
                         <div class="form-group">
                             <label for="inp-access-token">
                                 Wit.ai access token
                             </label>
-                            <div class="see-password-input">
-                                <i class="fa fa-eye" aria-hidden="true"></i>
-                                <input id="inp-access-token" class="" type="password">
+
+                            <div class="input-wrapper see-password-input">
+                                <i class="fa fa-eye password-eye-icon" aria-hidden="true"></i>
+                                <input id="inp-access-token" class="inp-loading" type="password" >
+                                <i class="fa fa-repeat input-saving-overlay hidden"></i>
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="inp-dev-token">
                                 Wit.ai developers token
                             </label>
-                            <div class="see-password-input">
-                                <i class="fa fa-eye" aria-hidden="true"></i>
+
+                            <div class="input-wrapper see-password-input">
+                                <i class="fa fa-eye password-eye-icon" aria-hidden="true"></i>
                                 <input id="inp-dev-token" class="" type="password">
+                                <i class="fa fa-repeat input-saving-overlay hidden"></i>
                             </div>
                         </div>
                     </div>
                     <div class="app-footer-section">
-                        <button class="main-btn">Embedden</button> <button class="main-btn">Selecteer</button>
+                        <button class="main-btn">Embedden</button>
+                        <button class="main-btn">Selecteer</button>
                     </div>
                 </div>
 
-                <div class="col-md-12 app-wrapper" >
+                <div class="col-md-12 app-wrapper">
                     <div class="app-top-section">
-                        <h2>App naam 1</h2> <span><i class="fa fa-times" aria-hidden="true"></i></span>
+                        <div class="input-wrapper disable-shrink">
+                            <input type="text" class='inp-head-name' value="App naam 1">
+                            <i class="fa fa-repeat input-saving-overlay hidden"></i>
+                        </div>
+                        <span><i class="fa fa-times " aria-hidden="true"></i></span>
+
                     </div>
                     <div class="app-center-section">
                         <div class="form-group">
                             <label for="inp-access-token">
                                 Wit.ai access token
                             </label>
-                            <div class="see-password-input">
-                                <i class="fa fa-eye" aria-hidden="true"></i>
-                                <input id="inp-access-token" class="" type="password">
+
+                            <div class="input-wrapper see-password-input">
+                                <i class="fa fa-eye password-eye-icon" aria-hidden="true"></i>
+                                <input id="inp-access-token" class="inp-loading" type="password" >
+                                <i class="fa fa-repeat input-saving-overlay hidden"></i>
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="inp-dev-token">
                                 Wit.ai developers token
                             </label>
-                            <div class="see-password-input">
-                                <i class="fa fa-eye" aria-hidden="true"></i>
+
+                            <div class="input-wrapper see-password-input">
+                                <i class="fa fa-eye password-eye-icon" aria-hidden="true"></i>
                                 <input id="inp-dev-token" class="" type="password">
+                                <i class="fa fa-repeat input-saving-overlay hidden"></i>
                             </div>
                         </div>
                     </div>
                     <div class="app-footer-section">
-                        <button class="main-btn">Embedden</button> <button class="main-btn">Selecteer</button>
+                        <button class="main-btn">Embedden</button>
+                        <button class="main-btn">Selecteer</button>
                     </div>
                 </div>
 
-                <div class="col-md-12 app-wrapper" >
+                <div class="col-md-12 app-wrapper">
                     <div class="app-top-section">
-                        <h2>App naam 1</h2> <span><i class="fa fa-times" aria-hidden="true"></i></span>
+                        <div class="input-wrapper disable-shrink">
+                            <input type="text" class='inp-head-name' value="App naam 1">
+                            <i class="fa fa-repeat input-saving-overlay hidden"></i>
+                        </div>
+                        <span><i class="fa fa-times " aria-hidden="true"></i></span>
+
                     </div>
                     <div class="app-center-section">
                         <div class="form-group">
                             <label for="inp-access-token">
                                 Wit.ai access token
                             </label>
-                            <div class="see-password-input">
-                                <i class="fa fa-eye" aria-hidden="true"></i>
-                                <input id="inp-access-token" class="" type="password">
+
+                            <div class="input-wrapper see-password-input">
+                                <i class="fa fa-eye password-eye-icon" aria-hidden="true"></i>
+                                <input id="inp-access-token" class="inp-loading" type="password" >
+                                <i class="fa fa-repeat input-saving-overlay hidden"></i>
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="inp-dev-token">
                                 Wit.ai developers token
                             </label>
-                            <div class="see-password-input">
-                                <i class="fa fa-eye" aria-hidden="true"></i>
+
+                            <div class="input-wrapper see-password-input">
+                                <i class="fa fa-eye password-eye-icon" aria-hidden="true"></i>
                                 <input id="inp-dev-token" class="" type="password">
+                                <i class="fa fa-repeat input-saving-overlay hidden"></i>
                             </div>
                         </div>
                     </div>
                     <div class="app-footer-section">
-                        <button class="main-btn">Embedden</button> <button class="main-btn">Selecteer</button>
+                        <button class="main-btn">Embedden</button>
+                        <button class="main-btn">Selecteer</button>
                     </div>
                 </div>
 
-                <div class="col-md-12 app-wrapper" >
+                <div class="col-md-12 app-wrapper">
                     <div class="app-top-section">
-                        <h2>App naam 1</h2> <span><i class="fa fa-times" aria-hidden="true"></i></span>
+                        <div class="input-wrapper disable-shrink">
+                            <input type="text" class='inp-head-name' value="App naam 1">
+                            <i class="fa fa-repeat input-saving-overlay hidden"></i>
+                        </div>
+                        <span><i class="fa fa-times " aria-hidden="true"></i></span>
+
                     </div>
                     <div class="app-center-section">
                         <div class="form-group">
                             <label for="inp-access-token">
                                 Wit.ai access token
                             </label>
-                            <div class="see-password-input">
-                                <i class="fa fa-eye" aria-hidden="true"></i>
-                                <input id="inp-access-token" class="" type="password">
+
+                            <div class="input-wrapper see-password-input">
+                                <i class="fa fa-eye password-eye-icon" aria-hidden="true"></i>
+                                <input id="inp-access-token" class="inp-loading" type="password" >
+                                <i class="fa fa-repeat input-saving-overlay hidden"></i>
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="inp-dev-token">
                                 Wit.ai developers token
                             </label>
-                            <div class="see-password-input">
-                                <i class="fa fa-eye" aria-hidden="true"></i>
+
+                            <div class="input-wrapper see-password-input">
+                                <i class="fa fa-eye password-eye-icon" aria-hidden="true"></i>
                                 <input id="inp-dev-token" class="" type="password">
+                                <i class="fa fa-repeat input-saving-overlay hidden"></i>
                             </div>
                         </div>
                     </div>
                     <div class="app-footer-section">
-                        <button class="main-btn">Embedden</button> <button class="main-btn">Selecteer</button>
-                    </div>
-                </div>
-
-                <div class="col-md-12 app-wrapper" >
-                    <div class="app-top-section">
-                        <h2>App naam 1</h2> <span><i class="fa fa-times" aria-hidden="true"></i></span>
-                    </div>
-                    <div class="app-center-section">
-                        <div class="form-group">
-                            <label for="inp-access-token">
-                                Wit.ai access token
-                            </label>
-                            <div class="see-password-input">
-                                <i class="fa fa-eye" aria-hidden="true"></i>
-                                <input id="inp-access-token" class="" type="password">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="inp-dev-token">
-                                Wit.ai developers token
-                            </label>
-                            <div class="see-password-input">
-                                <i class="fa fa-eye" aria-hidden="true"></i>
-                                <input id="inp-dev-token" class="" type="password">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="app-footer-section">
-                        <button class="main-btn">Embedden</button> <button class="main-btn">Selecteer</button>
+                        <button class="main-btn">Embedden</button>
+                        <button class="main-btn">Selecteer</button>
                     </div>
                 </div>
 
 
-                </div>
+
+
+
+
             </div>
-
         </div>
+
+    </div>
     </div>
 @endsection
